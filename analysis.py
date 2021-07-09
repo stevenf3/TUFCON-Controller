fcc95210a15f8cd8cd25d957e10fa6983c444376
stevenf3 <73904592+stevenf3@users.noreply.github.com>
2021-07-09 13:41:25 -0500

    analysis stuff, working with pandas now

--- analysis.py
+++ analysis.py
@@ -3,18 +3,18 @@ import numpy as np
 import csv
 import pandas as pd
 
-filename = '070821-Test1.csv'
-
-timelist = []
-GPList = []
-SSList = []
-RadList = []
-ConvectronList = []
-BaratronList = []
-IonList = []
-PowerList = []
-FlowRateList = []
+filename = '070921-100mT.csv'
 
 df = pd.read_csv(filename)
-
 print(df)
+#df.plot('Time', 'Radical Density', logy=True, ylim=(1e19, 1e21))
+timelist = df['Time'].tolist()
+RadList = df['Radical Density'].tolist()
+
+fig, ax = plt.subplots()
+ax.plot(timelist, RadList)
+ax.set_xlabel('Time')
+ax.set_ylabel('Radical Density')
+ax.set_yscale('log')
+ax.set_ylim(1e19, 1e21)
+plt.show()
