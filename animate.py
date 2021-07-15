5803d3bc46e2fbe64b3a45bec9ac25ecd4ec3fe3
stevenf3 <73904592+stevenf3@users.noreply.github.com>
2021-07-15 11:08:38 -0500

    data dumping working, have to get working with plot

--- animate.py
+++ animate.py
@@ -15,7 +15,6 @@ running = False
 df = pd.read_csv('070921-60mTorr.csv')
 GP = df['Gold Probe Temperature']
 SS = df['Stainless Steel Probe Temperature']
-print(df)
 
 class animation(tk.Tk):
     def __init__(self):
@@ -24,10 +23,12 @@ class animation(tk.Tk):
 
         self.running = False
         self.i=-1
+        self.iten = -1
         self.list = np.array([])
         self.timelist = np.array([])
         self.GoldProbeTempList = np.array([])
         self.SSProbeTempList = np.array([])
+        self.GPData = np.zeros((10,1))
 
         self.s = ttk.Style()
         self.s.configure('.', font=('Cambria'), fontsize=16)
@@ -79,25 +80,32 @@ class animation(tk.Tk):
         self.StopScan.grid_forget()
         self.StartScan.grid(row=0, columnspan=2,sticky='ew')
 
-
     def scanning(self):
-        if self.running:
-            tick = time.time()
-            print('running')
-            self.i += 1
-            print(self.i)
-            print(GP[self.i])
-            print(SS[self.i])
+        with open('tempwriter.csv', 'a') as file:
+            if self.running:
+                tick = time.time()
+                self.i += 1
+                self.iten += 1
+                if self.iten == 10:
+                    self.iten = 0
+                    np.savetxt(file, self.GPData)
+                    self.GPData = np.zeros((10,1))
+                print(self.i)
+                print(self.iten)
+                print(GP[self.i])
+                print(SS[self.i])
+                self.GPData[self.iten, 0] = GP[self.i]
+                print(self.GPData)
 
-            self.line1.set_xdata(self.i)
-            self.line1.set_ydata(GP[self.i])
-            self.canvas.draw()
-            tock = time.time()
-            delay = int(1000 * (tock - tick))
-        try:
-            self.after(1000 - delay, self.scanning)
-        except UnboundLocalError:
-            self.after(1000, self.scanning)
+                self.line1.set_xdata(self.i)
+                self.line1.set_ydata(GP[self.i])
+                self.canvas.draw()
+                tock = time.time()
+                delay = int(1000 * (tock - tick))
+            try:
+                self.after(1000 - delay, self.scanning)
+            except UnboundLocalError:
+                self.after(1000, self.scanning)
 
 if __name__ == '__main__':
     app = animation()
