a1e407306aae7a9452eea2d86a29c66f0a7aa5b7
Frank <stevenf3@illinois.edu>
2021-08-02 15:24:31 -0500

    fixed conbar in unstable

--- conbarconversion.py
+++ conbarconversion.py
@@ -11,34 +11,6 @@ df = pd.read_csv(filename)
 timelist = df['Time'].tolist()
 conlist = df['Convectron Pressure'].tolist()
 barlist = df['Baratron Pressure'].tolist()
-x = np.arange(0.001, 2, 0.01)
-y = x
-fig, ax = plt.subplots()
-
-savgol = savgol_filter(barlist, 13, 3)
-a, b = np.polyfit(conlist, savgol, 1)
-
-ystarlist = []
-for i in conlist:
-    ystar = a*i + b
-    ystarlist.append(ystar)
-
-y2 = barlist[-1]
-y1 = barlist[0]
-x2 = conlist[-1]
-x1 = conlist[0]
-
-slope2 = (y2-y1)/(x2-x1)
-print('slope:', a)
-print('slope2:', slope2)
-ax.plot(conlist,  savgol,  marker='o')
-ax.set_xlabel('convectron')
-ax.set_ylabel('baratron (true)')
-ax.set_yscale('log')
-ax.set_xscale('log')
-ax.plot(x,y)
-ax.plot(conlist, ystarlist)
-plt.show()
 
 def correct(val):
     correctedpressure = np.interp(val, conlist, barlist)
